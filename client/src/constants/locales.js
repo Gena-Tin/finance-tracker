075deb64d865fd71fe1764a459b5cb8598bea36f
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2026-04-29 20:35:01 +0300

    add locales

--- client/src/constants/locales.js
+++ client/src/constants/locales.js
@@ -1,5 +1,16 @@
-// src/constants/locales.js
 export const translations = {
+  ua: {
+    info: { flag: "🇺🇦", name: "Українська" },
+    data: {
+      title: "Трекер фінансів",
+      addOperation: "Додати операцію",
+      analytics: "Аналітика",
+      filters: "Фільтри",
+      balance: "Баланс",
+      lastOperations: "Останні операції",
+      chooseLanguage: "Оберіть мову",
+    },
+  },
   en: {
     info: {
       flag: "🇬🇧",
@@ -13,6 +24,55 @@ export const translations = {
       balance: "Balance",
       lastOperations: "Recent Transactions",
       chooseLanguage: "Choose Language",
+    },
+  },
+  pl: {
+    info: { flag: "🇵🇱", name: "Polski" },
+    data: {
+      title: "Tracker Finansowy",
+      addOperation: "Dodaj operację",
+      analytics: "Analityka",
+      filters: "Filtry",
+      balance: "Saldo",
+      lastOperations: "Ostatnie operacje",
+      chooseLanguage: "Wybierz język",
+    },
+  },
+  de: {
+    info: { flag: "🇩🇪", name: "Deutsch" },
+    data: {
+      title: "Finanz-Tracker",
+      addOperation: "Operation hinzufügen",
+      analytics: "Analytik",
+      filters: "Filter",
+      balance: "Kontostand",
+      lastOperations: "Letzte Operationen",
+      chooseLanguage: "Sprache wählen",
+    },
+  },
+  es: {
+    info: { flag: "🇪🇸", name: "Español" },
+    data: {
+      title: "Rastreador de Finanzas",
+      addOperation: "Añadir operación",
+      analytics: "Analityca",
+      filters: "Filtros",
+      balance: "Saldo",
+      lastOperations: "Últimas operaciones",
+      chooseLanguage: "Elegir idioma",
+    },
+  },
+
+  fr: {
+    info: { flag: "🇫🇷", name: "Français" },
+    data: {
+      title: "Suivi des Finances",
+      addOperation: "Ajouter une opération",
+      analytics: "Analytiques",
+      filters: "Filtres",
+      balance: "Solde",
+      lastOperations: "Dernières opérations",
+      chooseLanguage: "Choisissez la langue",
     },
   },
   ru: {
