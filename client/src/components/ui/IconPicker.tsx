9764677e91cb18fdd042539344605ff3d8c8067b
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2026-05-23 15:30:34 +0300

    migrate IconPicker to TypeScript

--- client/src/components/ui/IconPicker.tsx
+++ client/src/components/ui/IconPicker.tsx
@@ -1,10 +1,14 @@
 import styles from "./IconPicker.module.css";
 import { COMMON_ICONS } from "../../constants/icons";
 
-const IconPicker = ({ onSelect }) => {
+interface IconPickerProps {
+  onSelect: (emoji: string) => void;
+}
+
+const IconPicker: React.FC<IconPickerProps> = ({ onSelect }) => {
   return (
     <div className={styles.iconGrid}>
-      {COMMON_ICONS.map((emoji) => (
+      {COMMON_ICONS.map((emoji: string) => (
         <span
           key={emoji}
           className={styles.gridEmoji}
